3baba4b7baa3bf13da58c23416fd842c6a600ce4
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-07-27 23:02:53 +0200

    Create index route and controller

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = express();
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (_, response) => {
+  response.render('index', { title: 'Index' })
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`PS Project Running on port ${PORT}!`)
